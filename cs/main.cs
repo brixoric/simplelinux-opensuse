159a0de85265ab67caf2821cea47f25ed514fe5c
rollerskate1765 <205092395+brixoric@users.noreply.github.com>
2026-03-15 00:36:47 +0000

    added notes

--- cs/main.cs
+++ cs/main.cs
@@ -9,6 +9,11 @@ namespace GUIApp
             // TODO: 
             // Add .NET Windows Forms code to make a installer GUI.
             // Run each app one by one
+            //
+            // Notes for Windows Forms:
+            // Add label for progress
+            // Add requirements list
+            // Add a big legal disclaimer about how this is not an official installer and may cause data loss.
         }
     }
 }
